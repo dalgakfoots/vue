01bbd2f56d51b1a7d94a4c3d75ea09f41a84a853
dalgakfoots <hsw3074@gmail.com>
2023-03-21 11:23:47 +0900

    컴포넌트심화/events

---
+++ vue-tutorial/src/componentsAdvance/Events.vue
@@ -0,0 +1,33 @@
+<script>
+import EventsComponent from "./components/EventsComponent.vue";
+export default {
+  components: {
+    EventsComponent,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    callback() {
+      console.log("부모가 전달하는 메서드");
+    },
+    increaseCount(n) {
+      this.count += n;
+    },
+  },
+};
+</script>
+<template>
+  {{ count }}
+  <hr />
+  <!-- 자식 컴포넌트에서 설정한 emit의 이름을 prop으로 하여 전달  -->
+  <!-- camelCase 형식으로 이름을 설정했지만, kebab-case 표기로도 가능하며 이를 권장한다. -->
+  <EventsComponent @some-event="callback" />
+  <hr />
+  <!-- .once 수식어 가능 -->
+  <EventsComponent @some-event.once="callback" />
+  <hr />
+  <EventsComponent @increase-by="increaseCount" />
+</template>
---
+++ vue-tutorial/src/componentsAdvance/components/EventsComponent.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  methods: {
+    submit() {
+      this.$emit("someEvent");
+    },
+
+    submitForm(email, password) {
+      this.$emit("submit", { email, password });
+    },
+  },
+
+  emits: {
+    // 이벤트 유효성 검사
+    click: null, // 클릭은 유효성 검사 없음
+    submit: ({ email, password }) => {
+      // submit 이벤트 유효성 검사
+      if (email && password) {
+        return true;
+      } else {
+        console.warn("submit 이벤트 페이로드가 옳지 않음");
+        return false;
+      }
+    },
+  },
+};
+</script>
+<template>
+  <button @click="$emit('someEvent')">클릭하기</button>
+  <!-- 이벤트를 위한 인자 사용 -->
+  <button @click="$emit('increaseBy', 1)">Increase by 1</button>
+</template>
